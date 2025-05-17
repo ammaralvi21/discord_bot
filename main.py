5fbd0d69acc4410875c35e26009720b58c89f4f2
Ammar Alvi <ammaralvi21@gmail.com>
2025-05-16 23:07:35 -0700

    added wordle random word support

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import time
 from gtts import gTTS
 import datetime
+import random
+import asyncio
 
 
 load_dotenv()
@@ -17,6 +19,9 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+print('Fetching word list')
+WORDLE_WORDS = open("wordle.txt").read().splitlines()
+
 @client.event
 async def on_ready():
     print(f'{client.user=}')
@@ -27,6 +32,7 @@ async def on_ready():
         if guild.name == GUILD:
             break
     print(f'{client.user} is connected to the following guild: {guild.name}(id: {guild.id})')
+    client.loop.create_task(wordle_word())
     # myobj = gTTS(text=f"Hey there fuckers", lang='en', slow=False)
     # myobj.save("welcome.mp3")
     # #ch.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="welcome.mp3"))
@@ -70,24 +76,74 @@ def get_tts(text, filename):
 @client.event
 async def on_voice_state_update(member: Member, before: VoiceState, after: VoiceState):
     print(f"on_voice_state_update {member=}, {before=}, {after=}")
+    if member.nick is not None:
+        name = member.nick
+    else:
+        name = member
     if not before.channel and after.channel and member.id != client.user.id:
-        await join_play_audio(state=after, audio=get_tts(text=f"{member} has joined", filename="welcome"))
+        await join_play_audio(state=after, audio=get_tts(text=f"{name} has joined", filename="welcome"))
 
     if before.channel and not after.channel and member.id != client.user.id:
-        await join_play_audio(state=before, audio=get_tts(text=f"{member} has left", filename="goodbye"))
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} has left", filename="goodbye"))
 
-    if before.channel and after.channel and member.id != client.user.id:
-        await join_play_audio(state=before, audio=get_tts(text=f"{member} has left",   filename="goodbye"))
-        await join_play_audio(state=after,  audio=get_tts(text=f"{member} has joined", filename="welcome"))
+    if before.self_mute and not after.self_mute and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} has self unmuted", filename="unmuted"))
 
+    if not before.self_mute and after.self_mute and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} has self muted", filename="muted"))
+
+    if before.self_stream and not after.self_stream and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} stopped streaming", filename="stop_streaming"))
+
+    if not before.self_stream and after.self_stream and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} started streaming", filename="start_streaming"))
+
+    if before.afk and not after.afk and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} is back", filename="isnt_afk"))
+
+    if not before.afk and after.afk and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} is now AFK", filename="is_afk"))
+
+
+    if before.channel and after.channel and before.channel != after.channel and member.id != client.user.id:
+        await join_play_audio(state=before, audio=get_tts(text=f"{name} has left",   filename="goodbye"))
+        await join_play_audio(state=after,  audio=get_tts(text=f"{name} has joined", filename="welcome"))
+
+async def wordle_word():
+    last = time.time()
+    await client.wait_until_ready()
+    my_chan = None
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            for channel in guild.channels:
+                if channel.name == "scotts-miners":
+                    my_chan = channel
+                    print(f"Channel found {channel=}")
+                    break
+    while not client.is_closed():
+        curr_hr = datetime.datetime.now().hour
+        elapsed = time.time() - last
+        wait_s = 3*3600
+        schedule_hr = 4
+        print(f"Alive message, {curr_hr=}, {elapsed=}, {wait_s=}")
+        if curr_hr == schedule_hr and elapsed > wait_s:
+            last = time.time()
+            if my_chan is not None:
+                await my_chan.send(content=f'Today\'s random word: "{random.choice(WORDLE_WORDS)}"')
+            else:
+                print(f"Channel not found {client.guilds=}")
+                for guild in client.guilds:
+                    for channel in guild.channels:
+                        print(f"Available channels: {guild=} {channel=}")
+        await asyncio.sleep(20)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$deepthroat test poll'):
-        p = discord.Poll(question="Is this useful?", multiple=False, duration=datetime.timedelta(hours=1.0))
+    if message.content.startswith('$dt poll'):
+        p = discord.Poll(question="Play", multiple=False, duration=datetime.timedelta(hours=1.0))
         p.add_answer(text="Yes")
         p.add_answer(text="No")
         await message.channel.send(poll=p)
